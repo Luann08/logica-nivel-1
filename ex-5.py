a3e1cf85f6241325af1a5b051002d14097e1da76
Luann08 <luann082013@gmail.com>
2025-03-13 19:37:38 -0300

    Create ex-5.py

---
+++ ex-5.py
@@ -0,0 +1,11 @@
+#5. Tabela de notas - Você foi contratado por uma escola pra fazer o sistema de boletim dos alunos. Como primeiro passo, escreva um programa que produza a seguinte saída:
+
+print("""
+ALUNO(A)          NOTA
+=========         =====
+ALINE              9.0  
+MÁRIO              DEZ
+SÉRGIO             4.5    
+SHIRLEY            7.0
+
+""")
